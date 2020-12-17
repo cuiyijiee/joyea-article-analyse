4dc257bf05a1957717aea4bc1b79f90ca9799716
cuiyijie <cyj976655@gmail.com>
2020-12-17 17:17:15 +0800

    a

---
+++ src/main/java/me/cuiyijie/articleanalysis/ArticleAnalysisApplication.java
@@ -0,0 +1,13 @@
+package me.cuiyijie.articleanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/cuiyijie/articleanalysis/ArticleAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package me.cuiyijie.articleanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
